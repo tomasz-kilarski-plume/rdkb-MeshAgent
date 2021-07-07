b4983c7a6e176795e37aa96e6492ef8f57999d69
Steven Portley <Steven_Portley@comcast.com>
2021-07-07 17:27:50 -0400

    RDKB-36191 : Gtest integration in meshAgent
    Reason for change: Fix tests for meshAgent
    Risks: None
    Test Procedure: Make sure all test cases pass
    Signed-off-by: Steven Portley <Steven_Portley@comcast.com>
    
    (cherry picked from commit 445571a36174e3f26709ea3be429b186af4ecbb9)
    Change-Id: I9ef008012f714540fb38a2ce0ae69f9d887789f9

--- source/test/EthpodErrDet.cpp
+++ source/test/EthpodErrDet.cpp
@@ -1,21 +1,16 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include "ethpod_error_det.h"
-}
-
-TEST(EthPodErrDet, test123)
-{
-    ASSERT_EQ(1+1, 2);
+#include "MeshAgentSsp/ethpod_error_det.h"
 }
 
 TEST(EthPodErrDet, test1234)
 {
-    ASSERT_EQ(-1, meshHandleEvent("d0:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT));
-    ASSERT_EQ(0, meshAddPod("d0:6e:de:5e:65:dc"));
-    ASSERT_EQ(0, meshAddPod("d0:6e:de:5e:65:dc"));
-    ASSERT_EQ(0, meshAddPod("d0:6e:de:5e:65:dd"));
+    ASSERT_EQ(false, meshHandleEvent("d0:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT));
+    ASSERT_EQ(true, meshAddPod("d0:6e:de:5e:65:dc"));
+    ASSERT_EQ(true, meshAddPod("d0:6e:de:5e:65:dc"));
+    ASSERT_EQ(true, meshAddPod("d0:6e:de:5e:65:dd"));
 
-    ASSERT_EQ(0, meshHandleEvent("d0:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT)); //handle event okay
-    ASSERT_EQ(-1, meshHandleEvent("ab:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT)); //No pod with this mac
+    ASSERT_EQ(true, meshHandleEvent("d0:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT)); //handle event okay
+    ASSERT_EQ(false, meshHandleEvent("ab:6e:de:5e:65:dc", DHCP_ACK_PRIV_EVENT)); //No pod with this mac
 }
